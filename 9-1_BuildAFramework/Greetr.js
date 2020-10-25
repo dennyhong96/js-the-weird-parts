5c4eff08d34ac51421b4e39e8fe77ab955f3919a
Haiyang Hong <hong961127@gmail.com>
2020-10-25 01:14:17 -0700

    Setup prototype methods and chainable methods

--- 9-1_BuildAFramework/Greetr.js
+++ 9-1_BuildAFramework/Greetr.js
@@ -6,8 +6,57 @@
     return new Greetr.init(firstname, lastname, language);
   };
 
+  // "Private vars" that will not be exposed out side of Greetr
+  const SUPPORTED_LANGS = ["en", "es"];
+  const GREETINGS = { en: "Hello", es: "Hola" };
+  const FORMAL_GREETINGS = { en: "Greetings", es: "Saludos" };
+  const LOG_MSGS = { en: "Logged in", es: "Inicio sesion" };
+
   // Any methods user will be able to use on Greetr object later
-  Greetr.prototype = {};
+  // Define methods on prototype to save memory
+  Greetr.prototype = {
+    fullname() {
+      return `${this.firstname} ${this.lastname}`;
+    },
+    validate() {
+      // SUPPORTED_LANGS is accessable during runtime because of closure
+      if (!SUPPORTED_LANGS.includes(this.language)) {
+        throw "INVALID_LANGUAGE";
+      }
+    },
+    greeting() {
+      return `${GREETINGS[this.language]}, ${this.firstname}!`;
+    },
+    formalGreeting() {
+      return `${FORMAL_GREETINGS[this.language]}, ${this.fullname()}!`;
+    },
+    greet(formal) {
+      let msg;
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+      if (console) {
+        console.log(msg);
+      }
+      // `this` refers to the calling object at execution time
+      // makes the method chainable
+      return this;
+    },
+    log() {
+      // IE doesn't have a console unless console is open
+      if (console) {
+        console.log(`${LOG_MSGS[this.language]} : ${this.fullname()}`);
+      }
+      return this;
+    },
+    setLang(newLang) {
+      this.language = newLang;
+      this.validate(); // validate
+      return this;
+    },
+  };
 
   // Function constructors
   Greetr.init = function (firstname, lastname, language) {
--- 9-1_BuildAFramework/app.js
+++ 9-1_BuildAFramework/app.js
@@ -1,2 +1,2 @@
 var g = G$("John", "Doe");
-console.log(g);
+g.greet().greet(true).setLang("fr").greet();
